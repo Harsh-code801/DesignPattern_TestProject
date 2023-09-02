4ed16a9d8734d762b93c63faac6f523fc126508f
harsh <harsh@DESKTOP-2GQ2PKH>
2023-09-02 10:35:58 +0530

    Strategy Design Pattern

--- DesignPattern_TestProject/DesignPattern_TestProject/Program.cs
+++ DesignPattern_TestProject/DesignPattern_TestProject/Program.cs
@@ -1,10 +1,45 @@
 ﻿namespace DesignPattern_TestProject 
 {
+    #region Strategy Design Pattern
+    public interface VehicalFeature
+    {
+        void drive();
+    }
+    public class Sports : VehicalFeature
+    {
+        void VehicalFeature.drive()
+        {
+            Console.WriteLine("Sports Car");
+        }
+    }
+    public class Normal : VehicalFeature
+    {
+        void VehicalFeature.drive()
+        {
+            Console.WriteLine("Normal Car");
+        }
+    }
+    public class Vehical
+    {
+        VehicalFeature vehicalFeature;
+        public Vehical(VehicalFeature vehicalFeature)
+        {
+            this.vehicalFeature = vehicalFeature;
+        }
+        public void Drive()
+        {
+            vehicalFeature.drive();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vehical v1 = new Vehical(new Sports());
+            Vehical v2 = new Vehical(new Normal());
+            v1.Drive();
+            v2.Drive();
         }
-    }
+    } 
+    #endregion
 }
